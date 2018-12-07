222e608c9af720d74ec7557ce651aeb2e56b274c
Thau Nguyen <hoangthau627@gmail.com>
2018-12-07 16:29:38 +0700

    change callback host

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: "fun-english.auth0.com",
   clientId: "FpEo9WSZ3yjyIuJ2UdqM-LKhfLff6rhK",
-  callbackUrl: "https://1z0xj2m9pl.codesandbox.io/callback"
+  callbackUrl: "https://english4fun.now.sh/callback"
 };
